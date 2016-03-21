466dad84f4ccaad3a6d7c8f56c1adb7fb20ea3c0
Petar Stoyanov <tapetko@abv.bg>
2016-03-21 19:27:43 +0200

    Rewrite test change

---
+++ HM2/HomeworkDefiningClassesPart2/HomeworkDefiningClassesPart2/Classes/Path.cs
@@ -0,0 +1,26 @@
+﻿
+namespace HomeworkDefiningClassesPart2.Classes
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Path
+    {
+        private List<Point3D> listOfPoints;
+
+        public Path()
+        {
+            listOfPoints = new List<Point3D>();
+        }
+
+        public List <Point3D> ListOfPoints 
+        {
+            get { return listOfPoints; }
+        }
+
+        public void Add(Point3D point)
+        {
+            listOfPoints.Add(point);
+        }
+    }
+}
